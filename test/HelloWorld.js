b3f19dcc14e91a03713dd1267fb85f922019dc73
Sergii Bomko <aquila.ua@gmail.com>
2018-05-31 21:22:08 +0200

    updates test

--- test/HelloWorld.js
+++ test/HelloWorld.js
@@ -6,6 +6,6 @@ contract("HelloWorld", () => {
   it('sould return string', async () => {
     var contract = await HelloWorld.new();
     var result = await contract.get();
-    assert.equal(result, "Hello World1");
+    assert.equal(result, "Hello World");
   });
 });
